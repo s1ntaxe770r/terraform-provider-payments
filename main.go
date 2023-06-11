26d53e2d07b47a5f2e0cb9097138f59385df6f78
s1ntaxe770r <cyberdev01@protonmail.com>
2023-06-11 21:00:16 +0100

    more client stuff

--- main.go
+++ main.go
@@ -1,25 +1,36 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/s1ntaxe770r/terraform-provider-payments/payments"
+	"github.com/s1ntaxe770r/terraform-provider-payments/pkg/client"
+	"golang.org/x/exp/slog"
 )
 
 func main() {
-	var debugMode bool
+	// var debugMode bool
 
-	flag.BoolVar(&debugMode, "debug", false, "set to true to run provider with debug support")
+	// flag.BoolVar(&debugMode, "debug", false, "set to true to run provider with debug support")
 
-	flag.Parse()
+	// flag.Parse()
 
-	opts := &plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return payments.Provider()
-		},
+	// opts := &plugin.ServeOpts{
+	// 	ProviderFunc: func() *schema.Provider {
+	// 		return payments.Provider()
+	// 	},
+	// }
+
+	// plugin.Serve(opts)
+
+	c := client.NewClient("", "hello@jubril.xyz")
+
+	resp, err := c.GetBankList()
+
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	plugin.Serve(opts)
+	slog.Info(resp.Data.Banks[0].BankName)
+	slog.Info(resp.Data.Banks[0].BankCode)
+
 }
