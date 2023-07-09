dfed384816a0356abadef27c661ae9f518d3267b
s1ntaxe770r <cyberdev01@protonmail.com>
2023-07-09 14:26:42 +0100

    ammend example

--- examples/main.tf
+++ examples/main.tf
@@ -1,13 +1,22 @@
 terraform {
   required_providers {
     payments = {
-      source  = "jubril/custom/payments"
+      version = "0.1.0"
+      source  = "jubril.xyz/custom/payments"
     }
   }
 }
 
 provider "payments" {
-  api_token = "VprYNiCxdaEG9cwX3vqK"
-  email = "hello@jubril.xyz"
-  account_number = "3000763375"
+  api_token      = var.api_token
+  email          = var.email
+  account_number = var.account_number
 }
+
+data "payments_banks" "banks" {}
+
+output "banks" {
+  value = data.payments_banks.banks
+}
+
+
